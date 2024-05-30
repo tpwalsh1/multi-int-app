32b5b886af7c5b1db15f80bdbd257fae6f17cb0f
tpwalsh1 <141166673+tpwalsh1@users.noreply.github.com>
2024-05-30 17:25:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,21 +67,10 @@ if uploaded_file:
         # Send the request
         response = requests.post(endpoint_url, headers=headers, data=data_json)
 
-        
-
-        
-        st.write("Applying model to make predictions ...")
-        time.sleep(sleep_time)
-
-        
-        st.write("Displaying performance metrics ...")
-        time.sleep(sleep_time)
-
 # Check if the request was successful
 if response.status_code == 200:
     pred_df = pd.DataFrame(json.loads(response.content)['predictions'])
     anomalies_df = pred_df[pred_df['preds_str'] == 'Anomaly']
-    status.update(label="Status", state="Anomalies Found", expanded=False)
     with st.expander('Anomalies', expanded=True):
         st.dataframe(anomalies_df, height=210, use_container_width=True)
         # else:
